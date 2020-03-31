568b15ace4938c8b5f74c3426032d38958371aae
Nikita Konev <nikit.cpp@yandex.ru>
2020-03-31 12:28:46 +0300

    test prototypes

---
+++ src/test/java/name/nkonev/r2dbcmigrate/library/FutureTest.java
@@ -0,0 +1,20 @@
+package name.nkonev.r2dbcmigrate.library;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class FutureTest {
+
+    @Disabled
+    @Test
+    public void testThatTransactionsWrapsQueriesAndTransactionsAreNotNested() {
+
+    }
+
+    @Disabled
+    @Test
+    public void testSplitMigrationsFitsInMemory() {
+
+    }
+
+}
