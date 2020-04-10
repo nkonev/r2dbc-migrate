d3bae864a1b79c873b0bd8a8c6e40507a00f1b13
Nikita Konev <nikit.cpp@yandex.ru>
2020-04-10 18:56:15 +0300

    rm links

--- r2dbc-migrate-standalone/src/main/java/name/nkonev/r2dbc/migrate/standalone/R2dbcMigrateApplication.java
+++ r2dbc-migrate-standalone/src/main/java/name/nkonev/r2dbc/migrate/standalone/R2dbcMigrateApplication.java
@@ -9,15 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 //import reactor.tools.agent.ReactorDebugAgent;
 
-// https://github.com/spring-projects/spring-data-r2dbc
-// https://spring.io/blog/2019/12/06/spring-data-r2dbc-goes-ga
-// https://medium.com/w-logs/jdbc-for-spring-webflux-spring-data-r2dbc-99690208cfeb
-// https://www.infoq.com/news/2018/10/springone-r2dbc/
-// https://spring.io/blog/2018/12/07/reactive-programming-and-relational-databases
-// https://www.baeldung.com/r2dbc
-// https://simonbasle.github.io/2017/10/file-reading-in-reactor/
-// https://r2dbc.io/spec/0.8.1.RELEASE/spec/html/
-// https://www.infoq.com/news/2018/10/springone-r2dbc/
 @SpringBootApplication
 public class R2dbcMigrateApplication {
 
