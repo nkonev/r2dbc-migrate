73a936b5836d8593895afcc247763038bccd4d6e
Nikita Konev <nikit.cpp@yandex.ru>
2020-06-23 02:28:20 +0300

    use 42 magic number

--- r2dbc-migrate-core/src/main/java/name/nkonev/r2dbc/migrate/core/R2dbcMigrateProperties.java
+++ r2dbc-migrate-core/src/main/java/name/nkonev/r2dbc/migrate/core/R2dbcMigrateProperties.java
@@ -9,8 +9,8 @@ public class R2dbcMigrateProperties {
     private String resourcesPath;
     private int chunkSize = 1000;
     private Dialect dialect;
-    private String validationQuery = "select '1' as result";
-    private String validationQueryExpectedResultValue = "1";
+    private String validationQuery = "select '42' as result";
+    private String validationQueryExpectedResultValue = "42";
     private Duration validationQueryTimeout = Duration.ofSeconds(5);
     private Duration validationRetryDelay = Duration.ofSeconds(1);
     private Duration acquireLockRetryDelay = Duration.ofSeconds(1);
