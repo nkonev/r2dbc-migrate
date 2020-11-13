0c89abf772fb59286b050c1536b6f0e0b9c91383
Nikita Konev <nikit.cpp@yandex.ru>
2020-11-13 22:01:36 +0300

    update deprecated DatabaseClient

--- r2dbc-migrate-standalone/src/main/java/name/nkonev/r2dbc/migrate/standalone/R2dbcMigrateApplication.java
+++ r2dbc-migrate-standalone/src/main/java/name/nkonev/r2dbc/migrate/standalone/R2dbcMigrateApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.r2dbc.core.DatabaseClient;
 //import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
