608ad6b0b74290c787b3696d9f6d14b960090430
Shane <me@shanel.ee>
2021-12-13 20:45:18 +0000

    Add tests to cover semantic versions and update old test expectations

--- r2dbc-migrate-core/src/test/java/name/nkonev/r2dbc/migrate/core/FilenameParserTest.java
+++ r2dbc-migrate-core/src/test/java/name/nkonev/r2dbc/migrate/core/FilenameParserTest.java
@@ -10,7 +10,31 @@ class FilenameParserTest {
     void testSplit() {
         String s = "V5__create_customers__split.sql";
         FilenameParser.MigrationInfo migrationInfo = FilenameParser.getMigrationInfo(s);
-        Assertions.assertEquals(5, migrationInfo.getVersion());
+        Assertions.assertEquals("5", migrationInfo.getVersion());
+        Assertions.assertEquals(5.0, migrationInfo.findDoubleVersion());
+        Assertions.assertEquals("create customers", migrationInfo.getDescription());
+        Assertions.assertTrue(migrationInfo.isSplitByLine());
+        Assertions.assertTrue(migrationInfo.isTransactional());
+    }
+
+    @Test
+    void testItParsesSemanticVersions() {
+        String s = "V1.2.3__create_customers__split.sql";
+        FilenameParser.MigrationInfo migrationInfo = FilenameParser.getMigrationInfo(s);
+        Assertions.assertEquals("1.2.3", migrationInfo.getVersion());
+        Assertions.assertEquals(1.23, migrationInfo.findDoubleVersion());
+        Assertions.assertEquals("create customers", migrationInfo.getDescription());
+        Assertions.assertEquals("create customers", migrationInfo.getDescription());
+        Assertions.assertTrue(migrationInfo.isSplitByLine());
+        Assertions.assertTrue(migrationInfo.isTransactional());
+    }
+
+    @Test
+    void testItParsesVersionsWithSingleDecimals() {
+        String s = "V1.2__create_customers__split.sql";
+        FilenameParser.MigrationInfo migrationInfo = FilenameParser.getMigrationInfo(s);
+        Assertions.assertEquals("1.2", migrationInfo.getVersion());
+        Assertions.assertEquals(1.2, migrationInfo.findDoubleVersion());
         Assertions.assertEquals("create customers", migrationInfo.getDescription());
         Assertions.assertTrue(migrationInfo.isSplitByLine());
         Assertions.assertTrue(migrationInfo.isTransactional());
@@ -20,7 +44,8 @@ class FilenameParserTest {
     void testSplitNontransactional() {
         String s = "V5__create_customers__split,nontransactional.sql";
         FilenameParser.MigrationInfo migrationInfo = FilenameParser.getMigrationInfo(s);
-        Assertions.assertEquals(5, migrationInfo.getVersion());
+        Assertions.assertEquals("5", migrationInfo.getVersion());
+        Assertions.assertEquals(5.0, migrationInfo.findDoubleVersion());
         Assertions.assertEquals("create customers", migrationInfo.getDescription());
         Assertions.assertTrue(migrationInfo.isSplitByLine());
         Assertions.assertFalse(migrationInfo.isTransactional());
@@ -30,7 +55,8 @@ class FilenameParserTest {
     void testNontransactional() {
         String s = "V5__create_customers__nontransactional.sql";
         FilenameParser.MigrationInfo migrationInfo = FilenameParser.getMigrationInfo(s);
-        Assertions.assertEquals(5, migrationInfo.getVersion());
+        Assertions.assertEquals("5", migrationInfo.getVersion());
+        Assertions.assertEquals(5.0, migrationInfo.findDoubleVersion());
         Assertions.assertEquals("create customers", migrationInfo.getDescription());
         Assertions.assertFalse(migrationInfo.isSplitByLine());
         Assertions.assertFalse(migrationInfo.isTransactional());
@@ -40,7 +66,8 @@ class FilenameParserTest {
     void test() {
         String s = "V5__create_customers.sql";
         FilenameParser.MigrationInfo migrationInfo = FilenameParser.getMigrationInfo(s);
-        Assertions.assertEquals(5, migrationInfo.getVersion());
+        Assertions.assertEquals("5", migrationInfo.getVersion());
+        Assertions.assertEquals(5.0, migrationInfo.findDoubleVersion());
         Assertions.assertEquals("create customers", migrationInfo.getDescription());
         Assertions.assertFalse(migrationInfo.isSplitByLine());
         Assertions.assertTrue(migrationInfo.isTransactional());
