240a746e686f71599bdc96f990f7aef20ed2558b
Nikita Konev <nikit.cpp@yandex.ru>
2024-03-11 05:07:05 +0400

    fix comments

--- r2dbc-migrate-core/src/main/java/name/nkonev/r2dbc/migrate/core/BunchOfResourcesType.java
+++ r2dbc-migrate-core/src/main/java/name/nkonev/r2dbc/migrate/core/BunchOfResourcesType.java
@@ -2,13 +2,12 @@ package name.nkonev.r2dbc.migrate.core;
 
 public enum BunchOfResourcesType {
     /**
-     * Files named according to convention, e. g. V1__create_customers.sql
+     * Files named according to the convention, e.g. V1__create_customers.sql
      */
     CONVENTIONALLY_NAMED_FILES,
 
     /**
-     * Arbitrary named files, their versions will be created according their order.
-     * This option should be used carefully along with CONVENTIONALLY_NAMED_FILES due to version auto generation.
+     * Arbitrary named file, its version should be declared in {@link name.nkonev.r2dbc.migrate.core.BunchOfResourcesEntry}.
      */
     JUST_FILE
 }
